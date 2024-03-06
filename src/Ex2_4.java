bdc964c2f48aadad760b6551e3fc8b1bdf77bacf
lch94 <ckdgns1447@gmail.com>
2024-03-07 02:42:24 +0900

    javabaisc

---
+++ src/Ex2_4.java
@@ -0,0 +1,9 @@
+public class Ex2_4 {
+    public static void main(String[] args) {
+        int x= 5;
+        System.out.println(x); // 5
+
+        int y = 10;
+        System.out.println(y); // 10
+    }
+}
